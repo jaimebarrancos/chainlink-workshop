9511de4fe14ae19400a006526b959dabe151f177
Jaime Ribeiro Barrancos <jaime.barrancos@outlook.com>
2023-05-03 00:03:48 +0100

    Task 6 and 7 completed!
    Access usdc faucet
    uniswap v3 pool
    
    (also deployed token on mumbai)

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,9 +1,8 @@
 from ape import accounts, project
 
 def main():
-    dev = accounts.test_accounts[0]
+    dev = accounts.load("workshop")
 
-    #contract = project.Simple.deploy(sender=user)
     contract =  dev.deploy(project.Token, "MyToken", "TKN")
 
     print("the name is : " + contract.name())
---
+++ scripts/faucet_withdrawl.py
@@ -0,0 +1,7 @@
+from ape import project, accounts
+
+def main():
+    user = accounts.load("workshop")
+    faucet = project.Faucet.at("0xD8c338f520C2e9289cC645b58B7471988143aA59")
+
+    faucet.withdraw(sender=user)
